57a519d0309e1700a66517f761299295ed9d62c8
stefangeorgerobert <stefan_george_robert@yahoo.com>
2020-11-16 13:04:19 +0200

    remove cookies

--- src/App.js
+++ src/App.js
@@ -3,17 +3,9 @@ import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes';
 import ScrollToTop from './utils/ScrollToTop';
 import './assets/base.scss';
-import { useCookies } from 'react-cookie';
 
 class App extends Component {
   render() {
-    const [cookies, setCookie] = useCookies([
-      'homeAlertMessage',
-      'homeAlertSeverity']);
-  
-    setCookie('homeAlertMessage', '', { path: '/' });
-    setCookie('homeAlertSeverity', '', { path: '/' });
-
     return (
         <BrowserRouter basename="/audit-registry/">
           <ScrollToTop>
